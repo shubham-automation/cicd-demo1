c833297d75238ab7ef143b04dfc8eb6e12d1a7f5
shubham-automation <shubhamchaudhari2017@gmail.com>
2023-11-01 23:24:10 -0500

    released new version

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_hello(self):
-        my_html = f"<body style='background-color: #4bbee6;'><h1 style='color:  #0a0a0a;'>Blue Version: V1</h1>"
+        my_html = f"<body style='background-color: #6bc930;'><h1 style='color:  #0a0a0a;'>Green Version: V2</h1>"
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data.decode('utf-8'), my_html)
 
     def test_hello_name(self):
         name = 'Shubham'
-        my_html = f"<body style='background-color: #4bbee6;'><h1 style='color:  #0a0a0a;'>Hello {name} you are accessing the Blue Version of the app: V1</h1>"
+        my_html = f"<body style='background-color: #6bc930;'><h1 style='color:  #0a0a0a;'>Hello {name} you are accessing the Green Version of the app: V2</h1>"
         rv = self.app.get(f'/{name}')
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
@@ -26,3 +26,4 @@ if __name__ == '__main__':
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     unittest.main()
+    
